204b79be3e388444bf9a883bbf1574f597e239a5
ALX2-Engineer <john7ouma@gmail.com>
2023-05-11 11:31:40 -0700

    correction.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,19 +11,29 @@
  
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	ssize_t file, let, w;
+	char *text;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	text = malloc(letters);
+	if (text == NULL)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
-	close(fd);
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	let = read(file, text, letters);
+
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
 	return (w);
 }
